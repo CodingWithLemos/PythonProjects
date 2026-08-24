fa2579cb05ffe9ade4dbecf6988d048d34fbd169
CodingWithLemos <andrelemoscodes@gmail.com>
2026-08-24 15:19:33 +0100

    wordcount: add output sorting logic

--- wordcount.py
+++ wordcount.py
@@ -1,22 +1,20 @@
 # Word Frequency Counter Python Program
 
-# Import sys module
-#import sys
-
-# Read text content of sys.argv
-#text_content = sys.argv[1]
-
-text_content = input('Type your phrase:\n')
+# Prompt the user to insert text in the terminal:
+text_content = input('Paste your text:\n')
 
 # Split text by delimiters (, . ? and !)
 splitted = text_content.split()
 
-# Remove any leading or trailing punctuation
+# Remove any leading or trailing punctuation, all words lowercase
 trimmed = [ word.strip(',.;:\t?!').casefold() for word in splitted ]
 
-# Sort text strings by occurrence using dictionaries
+# Arrange text strings using dictionaries
 word_frequency_counter = { x: trimmed.count(x) for x in trimmed }
 
+# Sort by frequency
+word_freq_sorted = sorted(word_frequency_counter, key=word_frequency_counter.get, reverse=True)
+
 # print the results to the terminal as key-value pairs
-for k, v in word_frequency_counter.items():
-    print (k , v)
+for k in word_freq_sorted:
+    print(k, word_frequency_counter[k])
