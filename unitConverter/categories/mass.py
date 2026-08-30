0cb363d8b14b9b6c86088848dd4fe5df3141dfe7
Andre Lemos <andrelemoscodes@gmail.com>
2026-08-30 10:12:08 +0100

    unitConverter\categories\mass: add function body with various unit conversions

---
+++ unitConverter/categories/mass.py
@@ -0,0 +1,31 @@
+# Mass Module with Various Unit Conversions
+
+# Main module
+
+def main(conversion):
+    # function body
+    def kg_to_pounds(m):
+        lbs = m * 2.20462
+        return lbs
+
+    def pounds_to_kg(m):
+        kg = m / 2.20462
+        return kg
+
+    def kg_to_ounces(m):
+        oz = m * 35.274
+        return oz
+
+    def ounces_to_kg(m):
+        kg = m / 35.274
+        return kg
+
+    def pounds_to_ounces(m):
+        oz = m * 16
+        return oz
+
+    def ounces_to_pounds(m):
+        lbs = m / 16
+        return lbs
+
+    
