d486ed83936a8bdfadc5a75ab37dbf5d1004d733
Andre Lemos <andrelemoscodes@gmail.com>
2026-08-30 09:15:03 +0100

    unitConverter\categories\length: add match case body with function calls

--- unitConverter/categories/length.py
+++ unitConverter/categories/length.py
@@ -2,12 +2,12 @@
 
 # Main module
 def main(conversion):
-
+    # function body
     def kilometers_to_miles(d):
         miles = d / 1.60934
         return miles
 
-    def kilometers_to_nauticle_miles(d):
+    def kilometers_to_nautical_miles(d):
         nautical_miles = d / 1.852
         return nautical_miles
 
@@ -29,6 +29,28 @@ def main(conversion):
 
     
 # matching each function to conversion types input by user
+    match conversion:
+        case "km > mi":
+            print(f"Length in miles: {kilometers_to_miles(d):.2f}")
+
+        case "km > nm":
+            print(f"Length in nautical miles: {kilometers_to_nautical_miles(d):.2f}")
+
+        case "mi > km":
+            print(f"Length in kilometers: {miles_to_kilometers(d):.2f}")
+
+        case "mi > nm":
+            print(f"Length in nautical miles: {miles_to_nautical_miles(d):.2f}")
+
+        case "nm > km":
+            print(f"Length in kilometers: {nautical_miles_to_kilometers(d):.2f}")
+
+        case "nm > mi":
+            print(f"Length in miles: {nautical_miles_to_miles(d):.2f}")
+
+        case _:
+            print("Invalid conversion!")
+
     
 if __name__ == "__main__":
     import sys
