e73faa21fa88b232206afa49128cc2bc528085b8
CodingWithLemos <andrelemoscodes@gmail.com>
2026-09-22 16:55:50 +0100

    errorexcept: add function and exception blocks

--- errorexcept.py
+++ errorexcept.py
@@ -8,10 +8,16 @@ BRIT_BANDS = {
     'Black Sabbath' : 'are a British Heavy Metal Band'
 }
 
-# Declare a function for processing bands, venues and stuff
+# Declare a function for processing bands, venues and instruments
+def concert(band_name, band_genre, *venues, **instruments ):
+    description = f'{band_name} are a {band_genre} band.'
+    tour_venues = f'They are coming to: {venues}.'
+    line_up = f'They consist of: {instruments}.'
+
+    return description, tour_venues, line_up
 
 
-# Block of Try..except.finally statements
+# Block of Try..except..finally statements
 try:
     for k, v in BRIT_BANDS.items():
         print(k, v) # accesses and prints dict contents
@@ -22,4 +28,24 @@ except TypeError:
 except:
     print('An exception occurred.')
 finally:
-    print('Program exited successfuly.') 
+    print('Program exited successfuly.')
+
+try:
+    print(
+        concert('The Beatles', 'Pop', 
+                'London', 'Vienna', 'Stockholm', 
+                vocals='John Lennon', bass='Paul McCartney', guitar='George Harrison',
+                drums = 'Ringo Starr'
+            )
+    )
+    
+except NameError:
+    print('variable is not defined.')
+except TypeError:
+    print('variable is of invalid type. Should be a function call.')
+except ValueError:
+    print('Argument is not of correct type. Check order of arguments.')
+except:
+    print('An exception occurred.')
+finally:
+    print('Program exited successfuly.')
